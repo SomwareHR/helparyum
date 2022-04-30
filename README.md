400c6a2f7576266bd3166570983c2cce3392e0cd
SomwareHR <45540611+SomwareHR@users.noreply.github.com>
2022-04-30 15:46:24 +0200

    v.22.0430.12 Beta Helparyum
    Powershell script that generates a simple HTML help file from instructions written in a simple CSV file.

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # helparyum
- Powershell script to generate a simple HTML help file from instructions in CSV file
+
+Powershell script that generates a simple HTML help file from instructions written in a simple CSV file.
+
+
+
+# Usage
+
+1. Edit helparyum.csv
+2. Run helparyum.ps1
+3. Script will generate "helparyum.html" and "helparyum.zip"
+4. Upload "helparyum.html" and media files to web server -or-
+5. Send "helparyum.zip" via e-mail
+
+File "helparyum.html" contains comments and links to media files.
+
+"Media file" is any kind of file that can be opened by browser.
+
+See example.
+
+
+
+## Input
+
+CSV file [helparyum.csv](helparyum-input.jpg)
+
+Format: two fields HELPTEXT and HELPMEDIA. HELPTEXT contains instructions for end user. HELPMEDIA is an (optional) media file (image, video, PDF, ...) that could be opened to better explain instruction. If there is no media, state so with "NO-MEDIA" keyword. Media links that lead to external links stay as they are.
+
+
+
+## Output
+
++ HTML file [helparyum.html](helparyum-output.jpg)
++ ZIP file  [helparyum.zip](helparyum-zip.jpg)
+
+HTML file is named either "helparyum.html" (default) or $Title.html (replaces " " with "_") if you specify it as CLI parameter. This is intended for upload to web server.
+
+ZIP filename convention is the same as HTML. ZIP contains main HTML file and all local media files. This is intended for sending instructions via e-mail.
+
+
+
+## Command line parameters
+
+-Title ..... Title of a resulting help file; also a filename for resulting HTML and ZIP files; defaut = "Helparyum"
+-CSVfile ... File with instructions to read; default = "helparyum.csv"
+
+
+## Syntax
+
++ `helparyum.ps1` ... will open helparyum.csv and create helparyum.html and helparyum.zip
++ `helparyum.ps1 -Title "One two three four"` ... will open helparyum.csv and create "one_two_three_four.html" and "one_two_three_four.zip"
++ `helparyum.ps1 -Title "One two three four" -CSVfile "1234.csv"` ... will open "1234.csv" and create "one_two_three_four.html" and "one_two_three_four.zip"
+
+If you use space in `-Title` parameter (e.g. `-Title "One two three four"`), script will replace ` ` (space) with `_` (underscore) in HTML and ZIP filenames.
+
+
+
+# Misc
+
+## Filenames
+
+Powershell should accept UTF-8 filenames. HOWEVER.. life is sooo much simpler if one uses only [a-z], [A-Z] and [0-9] in filenames. It has only been ~50 years since computers were introduced to masses, give programmers a break, will you?
+
+One particular problem which Powershell will not be able to workaround is - `[` (left (square) bracket). Don't use `[` in filenames. I am not a programmer and I have a hard time dealing with that. It's comforting to see that 2 trillion dollar companies also have problems with it.
+
+
+## ToDo
+
+
+### High priority
+
++ generate .MD file
++ generate .list file with list of files to be uploaded to web server
++ ideally: generete a "ncftput" script
+
+
+### Low priority (if ever)
+
++ bulleted list in addition to numbered (optional parameter "-Bulleted")
++ download media from external links so they can be included in .ZIP
+
+
+
+###### Public domain images and videos taken from https://www.pexels.com
+
++ https://www.pexels.com/@oleg-magni/
++ https://www.pexels.com/@olenkasergienko/
++ https://www.pexels.com/@rethaferguson/
++ https://www.pexels.com/@swisshumanity-1686058/
+
+###### PDF taken from https://archive.org
+
++ https://ia802902.us.archive.org/25/items/VICMachineCodeMonitor/VIC_Machine_Code_Monitor_text.pdf
+
+
+
+###### Helparyum Version 22.0430.12 ... (C)2022 SomwareHR ... License: MIT
---
+++ helparyum.ps1
@@ -0,0 +1,160 @@
+# v.22.0430.12 Beta Helparyum
+
+param (
+  [Parameter ( Mandatory=$False , HelpMessage="CSV file with instructions")]
+  [ValidateNotNullOrEmpty()]
+  [string]$CSVfile="helparyum.csv",
+
+  [Parameter ( Mandatory=$False , HelpMessage="Title")]
+  [ValidateNotNullOrEmpty()]
+  [string]$Title="Helparyum"
+)
+
+Set-StrictMode -Version Latest
+$Script:DefaultErrorActionPreference = "Stop"
+# Set-Location (Split-Path $MyInvocation.MyCommand.Path)
+$ErrorActionPreference = $Script:DefaultErrorActionPreference
+# -----------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------ CONST/VAR
+#  .d8888b.                             888       888     888
+# d88P  Y88b                            888       888     888
+# 888    888                            888       888     888
+# 888         .d88b.  88888b.  .d8888b  888888    Y88b   d88P 8888b.  888d888
+# 888        d88""88b 888 "88b 88K      888        Y88b d88P     "88b 888P"
+# 888    888 888  888 888  888 "Y8888b. 888  888888 Y88o88P  .d888888 888
+# Y88b  d88P Y88..88P 888  888      X88 Y88b.        Y888P   888  888 888
+#  "Y8888P"   "Y88P"  888  888  88888P'  "Y888        Y8P    "Y888888 888
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+$CrLf                     = "`r`n"
+$TimeStampHuman           = (Get-Date -UFormat "%Y-%m-%d %H:%M")
+$CSVdelimiter             = "|"
+$NoMediaIndicator         = "NO-MEDIA"  # text in CSV file that indicates there is no media file to click on
+$HelpFileName             = $Title.Replace(" ","_").ToLower()
+$ZipFileName              = $HelpFileName+".zip"
+$HTMLFileName             = $HelpFileName+".html"
+$Advertisement            = "Created on $timeStampHuman with 'helparyum' https://github.com/SomwareHR/helparyum/"
+$HTMLfontFamily           = "Verdana"
+$HTMLfontSize             = "medium"
+$HTMLanchorColor          = "#5f9ea0"
+$HTMLanchorHoverColor     = "red"
+$HTMLcaptionColor         = "lightsteelblue"
+$HTMLcaptionHoverColor    = "darkslategrey"
+$HTMLtextColor            = "white"
+$HTMLheadingsColor        = "yellow"
+$HTMLhrStyle              = "gray 1px dashed"
+$HTMLbodyBackgroundFlavor = "bg-dark"
+$HTMLbootstrapCSSFlavor1  = "https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
+$HTMLbootstrapJSFlavor    = "https://maxcdn.bootstrapcdn.com/bootstrap/latest/js/bootstrap.min.js"
+$HTMLjqueryFlavor         = "https://ajax.googleapis.com/ajax/libs/jquery/latest/jquery.min.js"
+$HTMLitemDIV              = "      <DIV class='row'>"
+$HTML                     = $null
+
+$HTMLheader1 = @"
+
+<!doctype html>
+<html lang="hr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+  <meta name="description" content="Description of the page less than 150 characters">
+  <title>Naslov</title>
+  <link rel="icon" type="image/png" href="https://example.com/favicon.png">
+
+  <link rel="stylesheet" href="$HTMLbootstrapCSSFlavor1">
+
+  <STYLE type="text/css">
+    a, a:visited           { color: $HTMLanchorColor }
+    a:hover                { color: $HTMLanchorHoverColor }
+    body                   { color: $HTMLtextColor ; font-family: "$HTMLfontFamily" ; font-size: "$HTMLfontSize" ; background-color: "$HTMLbodyBackgroundFlavor" }
+    .figure-caption        { color: $HTMLcaptionColor }
+    .figure-caption:hover  { background-color: $HTMLcaptionHoverColor }
+    h1, h2, h3, h4, h5, h6 { color: $HTMLheadingsColor }
+    hr                     { border-top: $HTMLhrStyle; }
+  </STYLE>
+
+</head>
+<body>
+  <BODY class="container-fluid $HTMLbodyBackgroundFlavor">
+    <DIV class="text-left $HTMLbodyBackgroundFlavor" style="margin-bottom:0">
+      <H1>$Title</H1><br>
+    </DIV>
+
+    <DIV class="container-fluid text-wrap text-break text-lg-left">
+
+    <!-- ----------------------------------------------------------------------------------------------------------------------------- -->
+
+"@
+
+$HTMLfooter1 = @"
+
+  <!-- ----------------------------------------------------------------------------------------------------------------------------- END -->
+    </DIV>
+
+"@
+
+$HTMLfooter2 = @"
+  <!-- ----------------------------------------------------------------------------------------------------------------------------- -->
+    <script src="$HTMLjqueryFlavor"></script>
+    <script src="$HTMLbootstrapJSFlavor"></script>
+  </p><p class="small text-secondary">$Advertisement</p>
+  </BODY>
+</HTML>
+
+"@
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------ MAIN
+#  888b     d888        d8888 8888888 888b    888
+#  8888b   d8888       d88888   888   8888b   888
+#  88888b.d88888      d88P888   888   88888b  888
+#  888Y88888P888     d88P 888   888   888Y88b 888
+#  888 Y888P 888    d88P  888   888   888 Y88b888
+#  888  Y8P  888   d88P   888   888   888  Y88888
+#  888   "   888  d8888888888   888   888   Y8888
+#  888       888 d88P     888 8888888 888    Y888
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+$CSVData      = Import-Csv -Path $CSVfile -Delimiter $CSVdelimiter -Encoding "UTF8"
+$HTML=$HTML+$HTMLheader1
+FOR ($Counter = 0; $Counter -lt $CSVdata.Count; $Counter++) {
+  $text  = $CSVData[$Counter]."HELPTEXT".trim()
+  $media = $CSVData[$Counter]."HELPMEDIA".trim()
+  $Counter2 = $Counter+1
+  Write-Host "Adding $Counter2`: $text / $media"
+  IF ($media -eq $NoMediaIndicator)      # Is this a "NO-MEDIA" indicator in .CSV file?
+  {
+    $link = $null
+  }
+  ELSE
+  {
+    $link ="(<a href='$media' target='_blank' title='$text ($media)'>🔗</a>)"
+    IF ( ($media -match "https://") -OR ($media -match "http://") -OR ($media -match "ftp://") ) {
+      # For future release maybe. Download external files?
+    }
+    ELSE
+    {
+      Compress-Archive -DestinationPath $ZipFileName -CompressionLevel Optimal -Update $media
+    }
+  }
+  $HTML = $HTML + `
+    $HTMLitemDIV + "$Counter2`. $text $link" + "</div>" + $CrLf
+}
+$HTML=$HTML+$HTMLfooter1
+$HTML=$HTML+$HTMLfooter2
+
+$HTML | Out-File $HelpFileName".html"
+
+Compress-Archive -DestinationPath $ZipFileName -CompressionLevel Optimal -Update $HTMLFileName
+
+Write-Output "$CrLf`*** The end ***$CrLf`Open $HTMLFileName in browser or send $ZipFileName via e-mail"
+
+exit
